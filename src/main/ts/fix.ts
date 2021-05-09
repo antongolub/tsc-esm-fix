502231e87d3dcdf8e3756bad8d00cf78825094a8
Anton Golub <mailbox@antongolub.ru>
2021-05-09 21:57:10 +0300

    feat: add relativeModuleExt fix

--- src/main/ts/fix.ts
+++ src/main/ts/fix.ts
@@ -1,8 +1,8 @@
+import globby from 'globby'
 import {resolve} from 'path'
 
 import {IFixOptions, IFixOptionsNormalized} from './interface'
-import {asArray, readJson} from './util'
-import globby from "globby";
+import {asArray, read, readJson} from './util'
 
 export const DEFAULT_FIX_OPTIONS: IFixOptionsNormalized = {
   cwd: process.cwd(),
@@ -16,21 +16,48 @@ export const normalizeOptions = (opts?: IFixOptions): IFixOptionsNormalized => (
 
 export const findTargets = (tsconfig: string | string[], cwd: string): string[] =>
   asArray(tsconfig).reduce<string[]>((targets, file) => {
-    const outDir = readJson(resolve(cwd, file))?.compilerOptions?.outDir
+    const tsconfigJson = readJson(resolve(cwd, file))
+    const outDir = tsconfigJson?.compilerOptions?.outDir
+    const module = tsconfigJson?.compilerOptions?.module?.toLowerCase()
 
-    if (outDir) {
+    if (outDir && (module === 'es2020' || module === 'esnext')) {
       targets.push(outDir)
     }
 
     return targets
   }, [])
 
+export const fix = (contents: string, { relativeModuleExt }: IFixOptionsNormalized): string => {
+  let _contents = contents
+
+  if (relativeModuleExt) {
+    const ext = relativeModuleExt === true ? '.js' : relativeModuleExt
+    _contents = _contents.replace(/(\sfrom ["']\.\/[^"']+)(["'])/g, `$1${ext}$2`)
+  }
+
+  return _contents
+}
 
 export const applyFix = async (opts?: IFixOptions): Promise<void> => {
   const _opts = normalizeOptions(opts)
-  const targets = findTargets(_opts.tsconfig, _opts.cwd)
+  const { cwd, target, tsconfig, out = cwd } = _opts
+  const targets = target ? asArray(target) : findTargets(tsconfig, cwd)
   const patterns = targets.map((target) => `${target}/**/*.js`)
-  const files = await globby(patterns,{cwd: _opts.cwd, onlyFiles: true})
+  const files = await globby(patterns,{cwd: cwd, onlyFiles: true, absolute: true})
+  const outDir = resolve(cwd, out)
+
+  // console.log('out=', out)
+  // console.log('out=', out)
+  // console.log('outDir=', outDir)
+
+  files.forEach((name) => {
+    const nextName = name.replace(cwd, outDir)
+    const contents = read(name)
+    const _contents = fix(contents, _opts)
+
+    console.log('nextName=', nextName)
+    console.log('_contents=', _contents)
+  })
 
   console.log('files=', files)
 }
