c153200a4b235c92095cf691a0f04a5d9d663b9b
chinsun9 <chin_sung@naver.com>
2021-06-07 10:07:23 +0900

    tsc
    cd src/test/fixtures/ts-project && tsc -p tsconfig.es5.json &&  tsc -p tsconfig.es6.json

--- src/test/fixtures/ts-project/target/es6/index.js
+++ src/test/fixtures/ts-project/target/es6/index.js
@@ -1,6 +1,7 @@
 import { foo } from './foo';
 export * from './foo';
 export * from './baz';
+export * from './q/u/x';
 export const foobaz = foo + 'baz';
 export const dirname = __dirname;
 export const filename = __filename;
