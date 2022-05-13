45fe9aeab76da42df1befabdc0e95d7c3f20c989
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-05-13 14:22:22 +0200

    feat: support updating .tsx files (#75)

--- src/main/ts/fix.ts
+++ src/main/ts/fix.ts
@@ -180,7 +180,7 @@ export const fix = async (opts?: IFixOptions): Promise<void> => {
 
   const patterns =
     sources.length > 0
-      ? sources.map((src) => `${src}/**/*.ts`)
+      ? sources.map((src) => `${src}/**/*.{ts,tsx}`)
       : targets.map((target) => `${target}/**/*.js`)
 
   const names = await globby(patterns, {
