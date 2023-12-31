6f843cafe979a57f23b400c666a240655f953603
Anton Golub <antongolub@antongolub.com>
2023-12-31 12:16:40 +0300

    fix: use depseek to avoid comments and string literals affects

--- src/main/ts/fixes/index.ts
+++ src/main/ts/fixes/index.ts
@@ -1,6 +1,35 @@
+import {TFixer} from '../interface'
+
 export {fixBlankFiles} from './fix-blank-files'
-export {fixContents} from './fix-contents'
 export {fixFilenameVar, fixDirnameVar} from './fix-dirname-var'
 export {fixModuleReferences} from './fix-module-ref'
 export {fixSourceMapRef} from './fix-sourcemap-ref'
 export {fixDefaultExport} from './fix-default-export'
+
+import { fixBlankFiles } from './fix-blank-files'
+import { fixDefaultExport } from './fix-default-export'
+import { fixDirnameVar, fixFilenameVar } from './fix-dirname-var'
+import { fixModuleReferences } from './fix-module-ref'
+import { fixSourceMapRef } from './fix-sourcemap-ref'
+
+export const fixContents: TFixer = (
+  ctx
+) => {
+  const {options} = ctx
+
+  let _ctx = ctx
+
+  if (options.ext) _ctx = fixModuleReferences(_ctx)
+
+  if (options.dirnameVar) _ctx = fixDirnameVar(_ctx)
+
+  if (options.filenameVar) _ctx = fixFilenameVar(_ctx)
+
+  if (options.fillBlank) _ctx = fixBlankFiles(_ctx)
+
+  if (options.forceDefaultExport) _ctx = fixDefaultExport(_ctx)
+
+  if (options.sourceMap) _ctx = fixSourceMapRef(_ctx)
+
+  return _ctx
+}
